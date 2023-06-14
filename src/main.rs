c95aab1be7169fdcaa0742d91a828c6cf55339f6
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-06-14 09:55:03 +0200

    Scheduler
    First idea of a scheduler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use filasse::job::*;
+use filasse::scheduler::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sched = Scheduler::new(1);
+    let mut job1 = Job::new(1, 2);
+    let mut job2 = Job::new(1, 3);
+    let mut job3 = Job::new(1, 4);
+
+    sched.add_to_scheduler(&mut job1);
+    sched.add_to_scheduler(&mut job2);
+    sched.add_to_scheduler(&mut job3);
+
+    sched.add_to_queue();
+
+    sched.round_robin();
 }
