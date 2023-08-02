ed1704814d70634546984a3521e4f0349a8a5a11
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-08-02 16:38:15 +0200

    Algorithm & API
    * Adding Algorithm trait simplified from job
    * Formatting as API by using SchedulerUser
    * Using Mutex / Not working yet

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,25 +1,97 @@
+use std::{collections::VecDeque, sync::Mutex};
+
+use once_cell::sync::Lazy;
+
 /// Dummy struct for Scheduler
 /// Even not enough to properly run a real theaded program as a task queue should be necessary
-struct Scheduler {}
+struct Scheduler {
+    queue: VecDeque<Task>,
+    algorithm: Fifo,
+}
+
+trait Algorithm {
+    fn get_next_task(&self, queue: &mut VecDeque<Task>) -> Option<Task>;
+}
+
+struct Fifo {}
+impl Algorithm for Fifo {
+    fn get_next_task(&self, queue: &mut VecDeque<Task>) -> Option<Task> {
+        queue.pop_front()
+    }
+}
+
+pub struct SchedulerUser {
+    scheduler: Scheduler,
+}
+
+struct Job {
+    func: Box<dyn Fn() -> ()>,
+}
+enum Task {
+    // Creation State
+    New(Job),
+    // In Queue State, ready to be scheduled
+    Ready(Job),
+    // In run
+    Running(Job),
+    // Blocked by I/O
+    Blocked(Job),
+    // Father dead
+    Zombie(Job),
+    // Before to be free
+    Terminated(Job),
+}
+
+impl SchedulerUser {
+    pub fn create_task<F: Fn() -> () + 'static>(&mut self, my_func: F) {
+        self.scheduler.create_task(Task::New(Job {
+            func: Box::new(my_func),
+        }));
+    }
+    pub fn yield_task(&mut self) {
+        dbg!("blocked ?");
+        self.scheduler.yield_task();
+        // Run the next ready task!
+    }
+    fn set_algorithm(_algorithm: impl Algorithm) {}
+}
 
 impl Scheduler {
     /// Register a task to run
     /// Think `pthread_create` alike
-    pub fn create_task(&self, my_func: impl Fn() -> ()) {
+    fn create_task(&mut self, task: Task) {
         println!("Task created");
         // Should only register the task, not run it.
-        self.run_task(my_func);
+        // When add to queue
+        if let Task::New(job) = task {
+            // (job.func)();
+            self.queue.push_back(Task::Ready(job));
+        }
+        // self.run_task(task);
+        self.yield_task();
     }
 
     /// Run a given task. Not to be called directly by end-user.
-    fn run_task(&self, my_func: impl Fn() -> ()) {
-        my_func();
+    fn run_task(&self, task: Task) {
+        match task {
+            Task::Ready(job) => (job.func)(),
+            _ => (),
+        }
         self.destroy_task();
     }
 
     /// Yield: go back to scheduler to run one of the ready tasks.
-    pub fn yield_task(&self) {
+    fn yield_task(&mut self) {
         println!("Calling yield");
+        if let Some(_retrun) = self.algorithm.get_next_task(&mut self.queue) {
+            if let Task::Ready(job) = _retrun {
+                (job.func)();
+            } else {
+                dbg!("bhb");
+            }
+        } else {
+            dbg!("aha");
+        }
         // Run the next ready task!
     }
 
@@ -29,32 +101,42 @@ impl Scheduler {
     }
 }
 
-static SCHEDULER: Scheduler = Scheduler {};
+pub static SCHEDULER: Mutex<SchedulerUser> = Mutex::new(SchedulerUser {
+    scheduler: Scheduler {
+        queue: VecDeque::new(),
+        algorithm: Fifo {},
+    },
+});
+
+// static QUEUE: Mutex<Lazy<VecDeque<Task>>> = Mutex::new(Lazy::new(|| VecDeque::new()));
+
+unsafe impl Sync for Task {}
+unsafe impl Send for Task {}
 
 fn task1() -> () {
     println!("Executing task 1");
     // Make scheduler able to schedule another ready task
     // Yield can also be hidden into a lock
-    SCHEDULER.yield_task();
+    SCHEDULER.lock().unwrap().yield_task();
     println!("Executing task 1");
     // Creating a child task
-    SCHEDULER.create_task(task2);
+    SCHEDULER.lock().unwrap().create_task(task2);
     println!("Executing task 1");
     // Make scheduler able to schedule another ready task
-    SCHEDULER.yield_task();
+    SCHEDULER.lock().unwrap().yield_task();
     println!("Executing task 1");
 }
 
 fn task2() -> () {
     println!("Executing task 2");
-    SCHEDULER.yield_task();
+    SCHEDULER.lock().unwrap().yield_task();
     println!("Executing task 2");
-    SCHEDULER.create_task(|| {
+    SCHEDULER.lock().unwrap().create_task(|| {
         println!("Executing a sub task");
     });
     println!("Executing task 2");
 }
 
 fn main() {
-    SCHEDULER.create_task(task1);
+    SCHEDULER.lock().unwrap().create_task(task1);
 }
